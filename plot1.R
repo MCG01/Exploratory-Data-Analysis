4f066149a105e2b71a52ed1f3e7f6022d0465881
Mark Gray <mgray@dingo.com>
2016-09-30 15:09:00 +1000

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,21 @@
+
+##PLOT 1##
+
+#Load libraries [WORKS]
+library(data.table)
+
+#Load the data file 
+hpc <- fread("household_power_consumption.txt", na.strings = c("?"))
+
+#Create subset of required data
+subSethpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Construct the png file [WORKS]
+png(file="plot1.png", width = 480, height = 480, pointsize = 12, bg = "white")
+
+#Plot data 
+hist(subSet$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+#Close 
+dev.off()
+
